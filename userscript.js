5228ef7c19831f6a2c62fa782d67cee681cacf26
lafkpages <mail@lafkpages.tech>
2023-05-09 19:13:03 +0000

    Add german userscript German description

--- userscript.js
+++ userscript.js
@@ -1,16 +1,17 @@
 // ==UserScript==
-// @name         Secret Message Encoder
-// @name:de      Geheime Nachrichten Kodierer
-// @namespace    https://lafkpages.tech
-// @version      0.1
-// @description  Encodes a secret message inside another.
-// @author       LuisAFK
-// @match        *://*/*
-// @icon         https://secret-message-encoder.luisafk.repl.co/favicon96.png
-// @website      https://secret-message-encoder.luisafk.repl.co
-// @grant        none
-// @supportURL   https://github.com/lafkpages/sme/issues
-// @downloadURL  https://secret-message-encoder.luisafk.repl.co/dist/userscript.js
-// @run-at       document-idle
+// @name             Secret Message Encoder
+// @name:de          Geheime Nachrichten Kodierer
+// @namespace        https://lafkpages.tech
+// @version          0.1
+// @description      Encodes a secret message inside another.
+// @description:de   Kodiert eine geheime Nachricht in einer anderen.
+// @author           LuisAFK
+// @match            *://*/*
+// @icon             https://secret-message-encoder.luisafk.repl.co/favicon96.png
+// @website          https://secret-message-encoder.luisafk.repl.co
+// @grant            none
+// @supportURL       https://github.com/lafkpages/sme/issues
+// @downloadURL      https://secret-message-encoder.luisafk.repl.co/dist/userscript.js
+// @run-at           document-idle
 // ==/UserScript==
 
