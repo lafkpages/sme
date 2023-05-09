af81fd1ad90e143f3d04886ab7a53111e61ff153
lafkpages <mail@lafkpages.tech>
2023-05-09 08:50:04 +0000

    Put userscript in its own JS file

--- build.sh
+++ build.sh
@@ -45,20 +45,7 @@ fi
 
 # Copy to userscript
 echo -n "Building userscript... "
-cat > "$BUILD_DIR/$BUILD_FILE_USERSCRIPT" <<- EOM
-// ==UserScript==
-// @name         Secret Message Encoder
-// @name:de      Geheime Nachrichten Kodierer
-// @namespace    https://lafkpages.tech
-// @version      0.1
-// @description  Encodes a secret message inside another.
-// @author       LuisAFK
-// @match        *://*/*
-// @icon         https://secret-message-encoder.luisafk.repl.co/favicon96.png
-// @grant        none
-// ==/UserScript==
-
-EOM
+cat userscript.js > "$BUILD_DIR/$BUILD_FILE_USERSCRIPT"
 cat "$BUILD_DIR/$BUILD_FILE" >> "$BUILD_DIR/$BUILD_FILE_USERSCRIPT"
 echo "done"
 
---
+++ userscript.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Secret Message Encoder
+// @name:de      Geheime Nachrichten Kodierer
+// @namespace    https://lafkpages.tech
+// @version      0.1
+// @description  Encodes a secret message inside another.
+// @author       LuisAFK
+// @match        *://*/*
+// @icon         https://secret-message-encoder.luisafk.repl.co/favicon96.png
+// @grant        none
+// ==/UserScript==
+
