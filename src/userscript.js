7ae6a1c7a7882b7559ac6bd9d859bec1a6c3ccae
lafkpages <mail@lafkpages.tech>
2023-05-10 15:51:32 +0000

    Fix userscript icon, bump to v0.1.4

--- src/userscript.js
+++ src/userscript.js
@@ -2,12 +2,12 @@
 // @name             Secret Message Encoder
 // @name:de          Geheime Nachrichten Kodierer
 // @namespace        https://sme-siz.pages.dev
-// @version          0.1.3
+// @version          0.1.4
 // @description      Encodes a secret message inside another.
 // @description:de   Kodiert eine geheime Nachricht in einer anderen.
 // @author           LuisAFK
 // @match            *://*/*
-// @icon             https://sme-siz.pages.dev/favicon96.png
+// @icon             https://sme-siz.pages.dev/assets/favicon96.png
 // @website          https://sme-siz.pages.dev
 // @grant            none
 // @supportURL       https://github.com/lafkpages/sme/issues
