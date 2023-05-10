1212bae664ccefa73932746fcac56f5456999fe2
lafkpages <mail@lafkpages.tech>
2023-05-10 13:53:29 +0000

    Update userscript to use Cloudflare Pages URLs instead of Replit, bump userscript version to 0.1.3

--- src/userscript.js
+++ src/userscript.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name             Secret Message Encoder
 // @name:de          Geheime Nachrichten Kodierer
-// @namespace        https://lafkpages.tech
-// @version          0.1.2
+// @namespace        https://sme-siz.pages.dev/
+// @version          0.1.3
 // @description      Encodes a secret message inside another.
 // @description:de   Kodiert eine geheime Nachricht in einer anderen.
 // @author           LuisAFK
 // @match            *://*/*
-// @icon             https://secret-message-encoder.luisafk.repl.co/favicon96.png
-// @website          https://secret-message-encoder.luisafk.repl.co
+// @icon             https://sme-siz.pages.dev/favicon96.png
+// @website          https://sme-siz.pages.dev
 // @grant            none
 // @supportURL       https://github.com/lafkpages/sme/issues
-// @downloadURL      https://secret-message-encoder.luisafk.repl.co/dist/userscript.js
+// @downloadURL      https://sme-siz.pages.dev/dist/userscript.js
 // @run-at           document-idle
 // @license          GPLv3
 // ==/UserScript==
