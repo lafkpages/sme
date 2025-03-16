2d92c92737810d3e0420b6676b034bc0f84a6f29
lafkpages <luisafk@proton.me>
2025-03-16 20:22:24 +0100

    feat: char test indicator

--- src/charTest.ts
+++ src/charTest.ts
@@ -5,6 +5,8 @@ import { BitArray } from "./bitArray";
 let refWidth = 0;
 const charTests: HTMLElement[] = [];
 
+const charTestIndicator = document.getElementById("char-test-indicator")!;
+
 const cookies = parse(document.cookie);
 
 if (!cookies["charTestId"]) {
@@ -35,44 +37,59 @@ async function setupCharTest() {
 }
 
 async function doCharTest() {
-  if (!refWidth) {
-    throw new Error("Char test not set up");
-  }
-
-  console.debug("Char test start");
-
-  let charCode = 0;
-
-  const chars = new BitArray();
-
-  const startTime = performance.now();
-  while (charCode < 0xffff) {
-    for (let i = 0; i < 128; i++) {
-      charTests[i].textContent = `ab${String.fromCharCode(charCode + i)}cd`;
+  try {
+    if (!refWidth) {
+      throw new Error("Char test not set up");
     }
 
-    await zeroTimeout();
+    console.debug("Char test start");
 
-    for (let i = 0; i < 128; i++) {
-      const testWidth = charTests[i].getBoundingClientRect().width;
+    let charCode = 0;
 
-      if (testWidth === refWidth) {
-        const code = charCode + i;
-        console.debug("Char", code, "is invisible");
-        chars.set(BigInt(code), true);
+    const chars = new BitArray();
+
+    const startTime = performance.now();
+    while (charCode < 0xffff) {
+      for (let i = 0; i < 128; i++) {
+        charTests[i].textContent = `ab${String.fromCharCode(charCode + i)}cd`;
       }
+
+      await zeroTimeout();
+
+      for (let i = 0; i < 128; i++) {
+        const testWidth = charTests[i].getBoundingClientRect().width;
+
+        if (testWidth === refWidth) {
+          const code = charCode + i;
+          console.debug("Char", code, "is invisible");
+          chars.set(BigInt(code), true);
+        }
+      }
+
+      charCode += 128;
     }
+    const endTime = performance.now();
 
-    charCode += 128;
-  }
-  const endTime = performance.now();
+    console.debug("Char test end", endTime - startTime);
 
-  console.debug("Char test end", endTime - startTime);
+    if (chars.buffer) {
+      const resp = await fetch("/invis-chars", {
+        method: "POST",
+        body: chars.toUint8Array(8192),
+      });
 
-  if (chars.buffer) {
-    await fetch("/invis-chars", {
-      method: "POST",
-      body: chars.toUint8Array(8192),
-    });
+      if (resp.status === 201) {
+        charTestIndicator.className = "char-test-indicator-success";
+      } else if (resp.status === 503) {
+        charTestIndicator.className = "char-test-indicator-disabled";
+      } else {
+        throw new Error("Char test request failed");
+      }
+    } else {
+      charTestIndicator.className = "char-test-indicator-empty";
+    }
+  } catch (err) {
+    charTestIndicator.className = "char-test-indicator-fail";
+    throw err;
   }
 }
