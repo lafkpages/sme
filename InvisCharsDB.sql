ba517f029a50e9daed2a20a4b71dd74a57fc29f4
lafkpages <luisafk@proton.me>
2025-03-17 19:16:27 +0100

    feat: add character_availability view to DB

--- InvisCharsDB.sql
+++ InvisCharsDB.sql
@@ -1,16 +1,24 @@
 CREATE TABLE IF NOT EXISTS chars (
-    testId INTEGER NOT NULL,
-    charCode INTEGER NOT NULL
+  testId INTEGER NOT NULL,
+  charCode INTEGER NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS tests (
-    testId INTEGER PRIMARY KEY AUTOINCREMENT,
-    timestamp INTEGER NOT NULL,
-    browserName TEXT,
-    cpuArchitecture TEXT,
-    deviceModel TEXT,
-    deviceVendor TEXT,
-    engineName TEXT,
-    osName TEXT,
-    userAgent TEXT NOT NULL
+  testId INTEGER PRIMARY KEY AUTOINCREMENT,
+  timestamp INTEGER NOT NULL,
+  browserName TEXT,
+  cpuArchitecture TEXT,
+  deviceModel TEXT,
+  deviceVendor TEXT,
+  engineName TEXT,
+  osName TEXT,
+  userAgent TEXT NOT NULL
 );
+CREATE VIEW character_availability AS
+SELECT chars.charCode,
+  COUNT(DISTINCT tests.browserName) AS browsersCount,
+  COUNT(DISTINCT tests.osName) AS osCount,
+  COUNT(DISTINCT tests.deviceModel) AS devicesCount,
+  COUNT(DISTINCT tests.testId) AS totalTests
+FROM chars
+  INNER JOIN tests ON chars.testId = tests.testId
+GROUP BY chars.charCode;
