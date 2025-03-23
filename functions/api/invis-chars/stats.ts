a36fbcd37aebd4e84a2c991ae1a5efeb1f1f73ce
lafkpages <luisafk@proton.me>
2025-03-23 19:22:01 +0100

    feat: improve invis-chars stats page

--- functions/api/invis-chars/stats.ts
+++ functions/api/invis-chars/stats.ts
@@ -3,16 +3,6 @@ import { encode } from "@msgpack/msgpack";
 
 import { ensureAuthed } from "../../../src/functions/utils";
 
-type StatsKey = (typeof keys)[number];
-const keys = [
-  "browserNameCount",
-  "cpuArchitectureCount",
-  "deviceModelCount",
-  "deviceVendorCount",
-  "engineNameCount",
-  "osNameCount",
-] as const;
-
 const _onRequest: PagesFunction<Env> = async (context) => {
   if (context.env.DISABLE_INVIS_CHARS_ENDPOINT) {
     return new Response(null, { status: 503 });
@@ -27,44 +17,33 @@ const _onRequest: PagesFunction<Env> = async (context) => {
     "SELECT count(*) AS testsCount FROM tests",
   ).first<{ testsCount: number }>())!;
 
-  const statements: D1PreparedStatement[] = [];
-  for (const key of keys) {
-    statements.push(
-      context.env.InvisCharsDB.prepare(
-        `--sql
-        SELECT charCode, ${key} FROM charsStats WHERE ${key} > 0 ORDER BY ${key} DESC
+  const { results: stats } = await context.env.InvisCharsDB.prepare(
+    `--sql
+          SELECT
+            charCode,
+            browserNameCount,
+            cpuArchitectureCount,
+            deviceModelCount,
+            deviceVendorCount,
+            engineNameCount,
+            osNameCount,
+            (browserNameCount + cpuArchitectureCount + deviceModelCount + deviceVendorCount + engineNameCount + osNameCount) AS totalCount,
+            (browserNameCount * 3 + osNameCount * 2 + deviceModelCount) AS score
+          FROM charsStats WHERE totalCount > 0 ORDER BY charCode ASC
         `,
-      ),
-    );
-  }
+  ).all<{
+    charCode: number;
+    browserNameCount: number;
+    cpuArchitectureCount: number;
+    deviceModelCount: number;
+    deviceVendorCount: number;
+    engineNameCount: number;
+    osNameCount: number;
+    totalCount: number;
+    score: number;
+  }>();
 
-  const statsResults = await context.env.InvisCharsDB.batch<{
-    [K in "charCode" | StatsKey]: number;
-  }>(statements);
-
-  // @ts-expect-error
-  const stats: Record<StatsKey, [number, number][]> = {};
-  for (let i = 0; i < keys.length; i++) {
-    const key = keys[i];
-    const results = statsResults[i].results;
-
-    if (!results.length) {
-      console.warn("No stats results for", key);
-      continue;
-    }
-
-    stats[key] = [];
-
-    for (const row of results) {
-      stats[key].push([row.charCode, row[key]]);
-    }
-  }
-
-  return new Response(encode({ testsCount, stats }), {
-    headers: {
-      "content-type": "application/json",
-    },
-  });
+  return new Response(encode({ testsCount, stats }));
 };
 
 const enableCache = false;
